f79a662975df68886d09e1e881598ccc1b8ed7eb
Kiwi <vera.kreis@bzwu.ch>
2026-05-28 10:54:59 +0200

    new version, pictures fixed, titel screen fixed

--- script.js
+++
@@ -1,8 +0,0 @@
-function flyAway() {
-    const butterfly = document.querySelector('.butterfly-img');
-    
-    if (!butterfly.classList.contains('fly-away')) {
-        butterfly.classList.add('fly-away');
-        console.log("Consequences triggered...");
-    }
-}
